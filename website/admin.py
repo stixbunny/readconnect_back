b6f71f5c7a1d446c66d522fbbb3ee1dc1f08cdd1
stixbunny <milenko.castillo@gmail.com>
2023-10-24 01:27:28 -0300

    admin register

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Book, Author, Category, Review, ReadList, ToReadList
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Review)
